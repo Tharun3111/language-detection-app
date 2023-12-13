7183c2933724f036c89e92c4f544f13af177e5bd
Tharun Chowdary Malepati <55556423+Tharun3111@users.noreply.github.com>
2023-12-12 18:42:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,6 @@ def predict():
 
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get port from environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
+
